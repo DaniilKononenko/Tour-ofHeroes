282bfbb4029fefb9599e0d00846d5da791359c63
Daniil Kononenko <rise.di.o3@gmail.com>
2024-10-30 11:19:43 +0300

    Added DashboardComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeroesComponent } from "./heroes/heroes.component";
 import { MessagesComponent } from './messages/messages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeroesComponent, MessagesComponent],
+  imports: [
+    RouterOutlet,
+    HeroesComponent,
+    MessagesComponent,
+    DashboardComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
